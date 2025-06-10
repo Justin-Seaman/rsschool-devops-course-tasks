93b53b87e1024fdc9da0dc0bdadfe414771638c8
Justin Seaman <justin_seaman@outlook.com>
2025-06-10 12:54:50 -0400

    declare s3 encryption and versioning in config

--- s3.tf
+++ s3.tf
@@ -7,3 +7,17 @@ resource "aws_s3_bucket_ownership_controls" "jsrsstate_ownership" {
     object_ownership = "BucketOwnerEnforced"
   }
 }
+resource "aws_s3_bucket_server_side_encryption_configuration" "jsrsstate_encrypt" {
+  bucket  = aws_s3_bucket.jsrsstate_create.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+resource "aws_s3_bucket_versioning" "jsrsstate_versioning" {
+  bucket = aws_s3_bucket.jsrsstate_create.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
