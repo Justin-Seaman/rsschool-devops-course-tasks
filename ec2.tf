ba6f13f685ff6760ae78c2f2bc197322767272c6
Justin Seaman <justin_seaman@outlook.com>
2025-06-11 17:02:55 -0400

    Move ec2 to NAT Gateway

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "ubuntu" {
   ami           = "ami-004364947f82c87a0"
   instance_type = "t2.micro"
-
+  subnet_id     = aws_subnet.jsrs-az1-pub1.id
   tags = {
-    Name = "Ubuntu VM"
+    Name = "NAT_GW-Ubuntu"
   }
 }
