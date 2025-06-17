8457d358ff288cf4550736f6d921fcb38e5cf120
Justin Seaman <justin_seaman@outlook.com>
2025-06-17 01:58:14 -0400

    fmt

--- rds.tf
+++ rds.tf
@@ -1,15 +1,15 @@
 ephemeral "aws_ssm_parameter" "db_secret" {
-  arn = "arn:aws:ssm:us-east-2:584296377309:parameter/jsrssecrets/rds/master_password"
+  arn             = "arn:aws:ssm:us-east-2:584296377309:parameter/jsrssecrets/rds/master_password"
   with_decryption = true
 }
 data "aws_ssm_parameter" "db_secret" {
-  name            = var.db_secret_path
+  name = var.db_secret_path
 }
 data "aws_ssm_parameter" "db_user" {
-  name            = var.db_user_path
+  name = var.db_user_path
 }
 data "aws_ssm_parameter" "db_name" {
-  name            = var.db_name_path
+  name = var.db_name_path
 }
 resource "aws_db_instance" "k3_sql_db" {
   allocated_storage           = 10
@@ -21,7 +21,7 @@ resource "aws_db_instance" "k3_sql_db" {
   instance_class              = "db.t3.micro"
   username                    = data.aws_ssm_parameter.db_user.value
   password_wo                 = ephemeral.aws_ssm_parameter.db_secret.value
-  password_wo_version         = data.aws_ssm_parameter.db_secret.version 
+  password_wo_version         = data.aws_ssm_parameter.db_secret.version
   skip_final_snapshot         = true
   db_subnet_group_name        = aws_db_subnet_group.rds_subnet_group.name
   vpc_security_group_ids      = [aws_security_group.sec_grp-private.id]
@@ -36,6 +36,6 @@ resource "aws_db_subnet_group" "rds_subnet_group" {
   }
 }
 output "db_password_version" {
-  value = aws_db_instance.k3_sql_db.password_wo_version
+  value       = aws_db_instance.k3_sql_db.password_wo_version
   description = "The endpoint of the RDS instance"
 }
